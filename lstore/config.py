0368fe911a1819f6f6e08a77eae0826b0908fb62
Shuying Li <ebi4642@gmail.com>
2022-02-04 20:51:17 -0800

    Raise error when no capacity

--- lstore/config.py
+++ lstore/config.py
@@ -1,6 +1,16 @@
+# Global Setting
 # Static Global Variables
 # Will update later
 
 # Page size: 4096b
 
 # may move column variables in table.py after finishing table.py part
+
+
+INDIRECTION_COLUMN = 0
+RID_COLUMN = 1
+TIMESTAMP_COLUMN = 2
+SCHEMA_ENCODING_COLUMN = 3
+# STARTTIME_COLUMN = 4
+# LASTUPDATETIME_COLUMN = 5
+# UPDATETIME_COLUMN = 6
--- lstore/page.py
+++ lstore/page.py
@@ -4,13 +4,16 @@ class Page:
         self.num_records = 0
         self.data = bytearray(4096)
 
-    def has_capacity(self):                 # is this supposed to return an int or a bool
-        capacity = 4096 - self.num_records  # how many free indexes are available in data
+    def has_capacity(self):                 # is this supposed to return an int or a bool - bool will be find 
+        capacity = 4096 - self.num_records  # how many free indexes are available in data - Should we need to 4096/8 
         if capacity == 0:
-            return false
+            return False
         else:
-            return true
+            return True
 
     def write(self, value):                 # sets the an empty array index to value
-        self.data[self.num_Records] = value
-        self.num_records += 1
+        if has_capacity():
+            self.data[self.num_records] = value
+            self.num_records += 1
+        else:
+            raise ValueError('No capacity')
