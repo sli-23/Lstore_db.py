06a1fd45c4abdea84709caa7e19077bf85c1ff18
theHuangNumber <58153454+theHuangNumber@users.noreply.github.com>
2022-02-04 18:27:41 -0800

    Update page.py

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,16 @@
-
 class Page:
 
     def __init__(self):
         self.num_records = 0
         self.data = bytearray(4096)
 
-    def has_capacity(self):
-        pass
+    def has_capacity(self):                 # is this supposed to return an int or a bool
+        capacity = 4096 - self.num_records  # how many free indexes are available in data
+        if capacity == 0:
+            return false
+        else:
+            return true
 
-    def write(self, value):
+    def write(self, value):                 # sets the an empty array index to value
+        self.data[self.num_Records] = value
         self.num_records += 1
-        pass
-
