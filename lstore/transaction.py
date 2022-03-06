f12bff162f5f9c438d54bda14ba0bcfea86abc8c
theHuangNumber <58153454+theHuangNumber@users.noreply.github.com>
2022-03-06 03:34:15 -0800

    Update transaction.py
    Added real and pseudo code for general usage of read/write locks

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -1,5 +1,54 @@
 from lstore.table import Table, Record
 from lstore.index import Index
+import threading
+
+
+class Locks:
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.reading = False
+        self.writing = False
+
+
+    def readLock(self):         # get read lock
+        self.lock.acquire()
+
+        if self.writing:        # can't read if writing
+            self.lock.release()
+            return False
+        else:
+            self.reading = True
+            self.lock.release()
+            return True
+
+
+    def releaseReadLock(self):
+        self.lock.acquire()
+        self.reading = False
+        self.lock.release()
+
+
+    def writeLock(self):
+        self.lock.acquire()
+
+        if self.reading:        # if something is reading, can't write
+            self.lock.release()
+            return False
+        elif self.writing:      # if something else is writing, can't write
+            self.lock.release()
+            return False
+        else:
+            self.writing = True
+            self.lock.release()
+            return True
+
+
+    def releaseWriteLock(self):
+        self.lock.acquire()
+        self.writing = False
+        self.lock.release()
+
 
 class Transaction:
 
@@ -8,6 +57,7 @@ class Transaction:
     """
     def __init__(self):
         self.queries = []
+        self.locks = {}
         pass
 
     """
@@ -21,12 +71,37 @@ class Transaction:
         self.queries.append((query, args))
         # use grades_table for aborting
 
-    # If you choose to implement this differently this method must still return True if transaction commits or False on abort
+    # If you choose to implement this differently this method must still return True if transaction
+    # commits or False on abort
     def run(self):
+        for query, args in self.queries:
+            rid = ???                               # pseudo-code
+            lock = self.locks.get(rid, None)
+
+            if query == read_query:                 # pseudo-code, if query is to read
+                if lock == None:     # no lock
+                    if (get readLock == False):     # pseudo-code, if cannot get read lock, abort
+                        return self.abort()
+                    else:
+                        locks[rid] = 'r'            # mark hash map key for that rid as reading
+
+            elif query == write_query:              # pseudo-code, if query is to write
+                if lock == None:     # no lock
+                    if (get writeLock == False):    # pseudo-code, if cannot get write lock, abort
+                        return self.abort()
+                    else:
+                        locks[rid] = 'w'            # mark hash map key for that rid as writing
+                elif lock == 'reading':
+                    releaseReadLock()               # pseudo-code, release the read lock
+                    if (get writeLock == False):    # pseudo-code, if cannot get write lock, abort
+                        return self.abort()
+                    else:
+                        locks[rid] = 'w'            # mark hash map key for that rid as writing
+
         for query, args in self.queries:
             result = query(*args)
             # If the query has failed the transaction should abort
-            if result == False:
+            if not result:
                 return self.abort()
         return self.commit()
 
