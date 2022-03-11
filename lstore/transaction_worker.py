4df66ef6939faf21f89f8a9861beb994480c5c36
Wade <59711081+wadeyq@users.noreply.github.com>
2022-03-10 23:40:15 -0800

    Update transaction_worker.py

--- lstore/transaction_worker.py
+++ lstore/transaction_worker.py
@@ -10,23 +10,37 @@ class TransactionWorker:
         self.stats = []
         self.transactions = transactions
         self.result = 0
+        # new varible thread
+        self.thread = None
         pass
 
+    """
+    Done, Appends t to transactions
+    """
     def add_transaction(self, t):
         self.transactions.append(t)
 
     """
-    # Adds the given query to this transaction
-    # Example:
-    # q = Query(grades_table)
-    # t = Transaction()
-    # t.add_query(q.update, 0, *[None, 1, None, 2, None])
-    # transaction_worker = TransactionWorker([t])
+    Runs all transaction as a thread
     """
     def run(self):
+        pass
+        # here you need to create a thread and call __run
+    
+
+    """
+    Done, Waits for the worker to finish
+    """
+    def join(self):
+        self.thread.join()
+        pass
+
+    """
+    Done
+    """
+    def __run(self):
         for transaction in self.transactions:
             # each transaction returns True if committed or False if aborted
             self.stats.append(transaction.run())
         # stores the number of transactions that committed
         self.result = len(list(filter(lambda x: x, self.stats)))
-
